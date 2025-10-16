46267f4399361c959b7a91d09b47545050a68b17
hxy2312505023 <15509131111@163.com>
2025-10-16 17:34:31 +0800

    '10.16'

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="home-container">
-    <h1>Home</h1>
-    <Mobanyufa />
-    <Shuxingbangding />
-  </div>
+    <div class="home-container">
+        <h1>VUE3教学</h1>
+        <hr>
+        <Mobanyvfa></Mobanyvfa>
+        <Shuxingbangding></Shuxingbangding>
+        <Tiaojianxuanran></Tiaojianxuanran>
+    </div>
 </template>
 
 <script>
+import Mobanyvfa from '../components/mobanyvfa.vue';
 import Mobanyufa from '../components/mobanyvfa.vue';
 import Shuxingbangding from '../components/shuxingbangding.vue';
-export default {
-  name: 'Home',
-  components: {
-    Mobanyufa,
-    Shuxingbangding
+import Tiaojianxuanran from '../components/tiaojianxuanran.vue';
 
-  }
+export default {
+    name: 'Home',
+    components: {
+        Mobanyvfa,
+        Shuxingbangding,
+        Tiaojianxuanran
+    },
+
 }
 </script>
 
-<style scroped>
-.home-container {
-  position: relative;
-  z-index: 10;
+<style scoped>
+.home-container{
+    position: relative;
+    z-index: 10;
 }
-
-h1 {
-  z-index: 11;
-  color: aqua;
+h1{
+    z-index: 11;
+    color: aqua;
 }
 </style>
